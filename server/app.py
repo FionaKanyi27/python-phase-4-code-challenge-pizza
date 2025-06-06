2d64e48e1fb7aa8e90e94175883945734d68cada
Fiona <fionakanyi0@gmail.com>
2025-06-06 22:23:31 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -14,11 +14,66 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
-
 api = Api(app)
 
+class Restaurants(Resource):
+    def get(self):
+        restaurants = Restaurant.query.all()
+        return [restaurant.to_dict(only=('id', 'name', 'address')) for restaurant in restaurants], 200
+
+
+class RestaurantByID(Resource):
+    def get(self, id):
+        restaurant = Restaurant.query.filter_by(id=id).first()
+        if not restaurant:
+            return {"error": "Restaurant not found"}, 404
+        
+        return restaurant.to_dict(only=('id', 'name', 'address', 'restaurant_pizzas', 'restaurant_pizzas.id', 'restaurant_pizzas.price', 'restaurant_pizzas.pizza_id', 'restaurant_pizzas.restaurant_id', 'restaurant_pizzas.pizza', 'restaurant_pizzas.pizza.id', 'restaurant_pizzas.pizza.name', 'restaurant_pizzas.pizza.ingredients')), 200
+    
+    def delete(self, id):
+        restaurant = Restaurant.query.filter_by(id=id).first()
+        if not restaurant:
+            return {"error": "Restaurant not found"}, 404
+        
+        db.session.delete(restaurant)
+        db.session.commit()
+        return '', 204
+
+
+class Pizzas(Resource):
+    def get(self):
+        pizzas = Pizza.query.all()
+        return [pizza.to_dict(only=('id', 'name', 'ingredients')) for pizza in pizzas], 200
+
+
+class RestaurantPizzas(Resource):
+    def post(self):
+        data = request.get_json()
+        
+        try:
+            new_restaurant_pizza = RestaurantPizza(
+                price=data['price'],
+                pizza_id=data['pizza_id'],
+                restaurant_id=data['restaurant_id']
+            )
+            
+            db.session.add(new_restaurant_pizza)
+            db.session.commit()
+            
+            return new_restaurant_pizza.to_dict(only=('id', 'price', 'pizza_id', 'restaurant_id', 'pizza', 'pizza.id', 'pizza.name', 'pizza.ingredients', 'restaurant', 'restaurant.id', 'restaurant.name', 'restaurant.address')), 201
+            
+        except ValueError:
+            return {"errors": ["validation errors"]}, 400
+        except Exception:
+            return {"errors": ["validation errors"]}, 400
+
+
+# Register resources
+api.add_resource(Restaurants, '/restaurants')
+api.add_resource(RestaurantByID, '/restaurants/<int:id>')
+api.add_resource(Pizzas, '/pizzas')
+api.add_resource(RestaurantPizzas, '/restaurant_pizzas')
 
 @app.route("/")
 def index():
